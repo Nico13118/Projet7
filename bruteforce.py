59fad99fa0c137bfcee8abe5a58bf4d1e2204d44
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-12-11 14:54:10 +0100

    Dans la fonction calculate_percentage, ajout dans le return la variable get_cost_action. Dans la boucle while True dans la condition if nbr_temp à True, ajout d'une condition if si la variable achat_action est superieur à MAX afin d'afficher le résultat.

--- bruteforce.py
+++ bruteforce.py
@@ -20,21 +20,36 @@ def calculate_percentage(get_i):
     get_cost_action = int(get_action["CoutParAction"])  # Récupération du cout de l'action
     get_pourcentage = float(get_action["PourcentageBenefice"])  # Récupération du pourcentage
     get_profit_result = get_cost_action * get_pourcentage
-    return get_action, get_profit_result
+    return get_action, get_profit_result, get_cost_action
 
 
 while True:
     i = random.randint(0, 19)
     if nbr_temp:
         if i not in nbr_temp:
-            j = list_actions[i]
-            nbr_temp.append(i)
-            select_action.append(j)
+            action, profit_result, cost_action = calculate_percentage(i)
+            achat_action += cost_action
+            if achat_action > MAX:
+                achat_action -= cost_action
+                print("Liste des actions selectionnées: ")
+                for select_action1 in select_action:
+                    print(select_action1["NomAction"],
+                          "Coût par action :", select_action1["CoutParAction"],
+                          "Bénéfice :", select_action1["PourcentageBenefice"])
+                print("Total des actions: ", achat_action)
+                print("Total des bénéfices : ", benefice)
+                break
+            if achat_action <= MAX:
+                benefice += profit_result
+                nbr_temp.append(i)
+                select_action.append(action)
+
     if not nbr_temp:
-        action, profit_result = calculate_percentage(i)
+        action, profit_result, cost_action = calculate_percentage(i)
         benefice = profit_result
         nbr_temp.append(i)
         select_action.append(action)
+        achat_action += cost_action
 
 
 # k = achat_action * pourcentage

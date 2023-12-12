5eb8b527162c74b0d7e0c73a37a268632898af0b
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2023-12-12 08:17:51 +0100

    Ajout de la variable random_search et info_max de type boolean. Ajout d'une variable benefice_max à 0. Création d'une fonction qui gère l'affichage du résultat. Création d'une fonction qui contrôle si présence d'une égalité en benefice et benefice_max ou si benefice et superieur ou inferieur à benefice_max et retourne la variable benefice, info_max et random_search. Dans la boucle while, ajout de la condition if not random_search qui permet de mettre fin à la boucle, la condition if not info_max qui permet de réinitialiser les données concernant les variable select_action, nbr_temp, achat_action et benefice

--- bruteforce.py
+++ bruteforce.py
@@ -4,10 +4,14 @@ import random
 
 path = os.getcwd()
 MAX = 500
+random_search = True
+info_max = True
 select_action = []  # Liste des actions selectionnées
 achat_action = 0  # Total des actions achetées
 benefice = 0  # Total des bénéfices
-nbr_temp = []  #
+benefice_max = 0
+nbr_temp = []  # Liste temporaire des index
+
 csv_path = os.path.join(path, "Actions.csv")
 with open(csv_path, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -23,7 +27,37 @@ def calculate_percentage(get_i):
     return get_action, get_profit_result, get_cost_action
 
 
-while True:
+def show_result():
+    print("Liste des actions selectionnées: ")
+    for select_action1 in select_action:
+        print(select_action1["NomAction"],
+              "Coût par action :", select_action1["CoutParAction"],
+              "Bénéfice :", select_action1["PourcentageBenefice"])
+    print("Total des actions: ", achat_action)
+    print("Total des bénéfices : ", benefice)
+    return False
+
+
+def profit_control(benefice_max1):
+    benefice_max1 = float(benefice_max1)
+    if benefice_max1:
+        if benefice == benefice_max1:
+            show_result()
+            info_max1 = False
+            random_search1 = False
+            return benefice, info_max1, random_search1
+        if benefice > benefice_max1:
+            info_max1 = False
+            return benefice, info_max1, random_search
+        if benefice < benefice_max1:
+            info_max1 = False
+            return benefice, info_max1, random_search
+    else:
+        info_max1 = False
+        return benefice, info_max1, random_search
+
+
+while random_search:
     i = random.randint(0, 19)
     if nbr_temp:
         if i not in nbr_temp:
@@ -31,25 +65,29 @@ while True:
             achat_action += cost_action
             if achat_action > MAX:
                 achat_action -= cost_action
-                print("Liste des actions selectionnées: ")
-                for select_action1 in select_action:
-                    print(select_action1["NomAction"],
-                          "Coût par action :", select_action1["CoutParAction"],
-                          "Bénéfice :", select_action1["PourcentageBenefice"])
-                print("Total des actions: ", achat_action)
-                print("Total des bénéfices : ", benefice)
-                break
+                info_benefice, info_max, random_search = profit_control(benefice_max)
+                if benefice > benefice_max:
+                    benefice_max = info_benefice
             if achat_action <= MAX:
-                benefice += profit_result
-                nbr_temp.append(i)
-                select_action.append(action)
+                if info_max:
+                    benefice += profit_result
+                    nbr_temp.append(i)
+                    select_action.append(action)
 
     if not nbr_temp:
+        info_max = True
         action, profit_result, cost_action = calculate_percentage(i)
         benefice = profit_result
         nbr_temp.append(i)
         select_action.append(action)
         achat_action += cost_action
+    if not random_search:
+        random_search = False
+    if not info_max:
+        select_action.clear()
+        nbr_temp.clear()
+        achat_action = 0
+        benefice = 0
 
 
 # k = achat_action * pourcentage
